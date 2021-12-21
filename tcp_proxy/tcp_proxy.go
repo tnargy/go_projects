25715bec03a6cb944341f966cc439e480bb2773a
Onyx <onyx@darkstar.local>
2021-12-21 11:40:40 -0600

    Changed to using io.Copy in echo

--- tcp_proxy/tcp_proxy.go
+++ tcp_proxy/tcp_proxy.go
@@ -8,23 +8,9 @@ import (
 
 func echo(conn net.Conn) {
 	defer conn.Close()
-	b := make([]byte, 512)
-	for {
-		size, err := conn.Read(b[0:])
-		if err == io.EOF {
-			log.Println("Client Disconnected")
-			break
-		}
-		if err != nil {
-			log.Println("Unexpected error")
-			break
-		}
-		log.Printf("Received %d bytes: %s\n", size, string(b))
 
-		log.Println("Writing data")
-		if _, err := conn.Write(b[0:size]); err != nil {
-			log.Fatalln("Unable to write data")
-		}
+	if _, err := io.Copy(conn, conn); err != nil {
+		log.Fatalln("Unable to read/write data.")
 	}
 }
 
