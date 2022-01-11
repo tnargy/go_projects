f5e8c5270e9f78e163a0889f4fda295d0861e979
Onyx <onyx@darkstar.local>
2022-01-11 13:16:58 -0600

    Simple Middleware using gorrila and negroni

--- http_server/simple.go
+++ http_server/simple.go
@@ -1,27 +1,47 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
 )
 
-type logger struct {
-	Inner http.Handler
+type badAuth struct {
+	Username string
+	Password string
 }
 
-func (l *logger) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	log.Println("start")
-	l.Inner.ServeHTTP(w, req)
-	log.Println("finish")
+type trivial struct {
 }
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(w, "Hello\n")
+func (b *badAuth) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	username := r.URL.Query().Get("username")
+	password := r.URL.Query().Get("password")
+	if username != b.Username || password != b.Password {
+		http.Error(w, "Unauthorized", 401)
+		return
+	}
+	ctx := context.WithValue(r.Context(), "username", username)
+	r = r.WithContext(ctx)
+	next(w, r)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	username := r.Context().Value("username").(string)
+	fmt.Fprintf(w, "Hi %s\n", username)
 }
 
 func main() {
-	f := http.HandlerFunc(hello)
-	l := logger{Inner: f}
-	http.ListenAndServe(":8000", &l)
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", hello).Methods("GET")
+	n := negroni.Classic()
+	n.Use(&badAuth{
+		Username: "admin",
+		Password: "password",
+	})
+	n.UseHandler(r)
+	http.ListenAndServe(":8000", n)
 }
